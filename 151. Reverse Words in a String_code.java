4210802650b24a80486c4ee8250d83ccf675e4b7
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-02-25 20:09:42 +0530

    Update 151. Reverse Words in a String_code.java

--- 151. Reverse Words in a String_code.java
+++ 151. Reverse Words in a String_code.java
@@ -1,7 +1,21 @@
 class Solution {
     public String reverseWords(String s) {
+            s=s.trim();
+           String [] str=s.split(" ");
+           String  result="";
             
-            String [] str=s.split(" ");
+           int n=str.length-1;
+            for(int i=n;i>=0;i--)
+            {
+                    if(str[i].isEmpty())
+                    {
+                      continue;
+                    }
+                result=result+str[i]+" ";
+                    
+                    
+            }
+            return result.trim();
             
             
     }
