3fcce138d05c46b7b33065ad72fa11d06c62472b
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-03-07 19:26:13 +0530

    Create 58. Length of Last Word_code.cpp

---
+++ 58. Length of Last Word_code.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+            int n=s.size()-1;
+            bool temp=false;
+            int c=0;
+            
+            for(int i=n;i>=0;i--)
+            {
+                    if(s[i]>='a' && s[i] <='z' ||  s[i]>='A' && s[i] <='Z')
+                    {
+                           temp=true;
+                            c++;
+                            
+                    }
+                    else{
+                            if(temp==true)
+                            return c;
+                    }
+            }
+                 return c;
+            
+        
+    }
+};
