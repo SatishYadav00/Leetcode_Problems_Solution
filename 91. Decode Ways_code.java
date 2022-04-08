8535b41a258bf84ff432f95890f4732e5cee7d24
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-04-08 22:45:16 +0530

    Create 91. Decode Ways_code.java

---
+++ 91. Decode Ways_code.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numDecodings(String s) {
+            if(s==null || s.charAt(0) == '0')
+                    return 0;
+            
+            int n=s.length();
+            int [] ar=new int[n+1];
+            ar[0]=1;
+            ar[1]=1;
+            
+            for(int i=2;i<=n;i++)
+            {
+                    int first=Integer.parseInt(s.substring(i-1,i));
+                    
+                    int second=Integer.parseInt(s.substring(i-2,i));
+          if(first >=1 && first<=9)
+          {
+                  
+                    ar[i] += ar[i-1];
+            }
+                  if(second >=10 && second<=26)
+          {
+                  
+                    ar[i] += ar[i-2];
+            }    
+            }
+            return ar[n];
+        
+   }
+}
