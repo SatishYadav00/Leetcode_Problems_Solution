b2a94ac9c6a6ec71fb4ea854e7bcffe7c6b3c487
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-04-17 22:45:41 +0530

    Create 81. Search in Rotated Sorted Array II_code.java

---
+++ 81. Search in Rotated Sorted Array II_code.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        
+            int low=0;
+            int high=nums.length-1;
+            
+            while(low<=high)
+            {
+                    if(nums[low]==target || nums[high]==target)
+                            return true;
+                            
+                   else if(target>nums[low])
+                            {
+                              while(low<high && nums[low+1] ==nums[low] )
+                              {
+                                 low++ ;
+                              }
+                                    low++ ;
+                            }
+                             else if(target<nums[high])
+                            {
+                              while(low<high && nums[high-1]==nums[high])
+                              {
+                                 high-- ;
+                              }
+                                    high-- ;
+                            }
+                    else
+                    {
+                            break;
+                    }
+                            
+                                            
+            }
+                            return false;
+    }
+}
