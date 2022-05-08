998df66efb69fe74f72781d3a3d16b50898a9433
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-05-08 19:55:16 +0530

    Create Happy Number_code.java

---
+++ Happy Number_code.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+           Set<Integer> s=new HashSet<>();
+            while( n!=1 && !s.contains(n))
+            {
+                  s.add(n);
+                    int sum=0;
+                    while(n>0)
+                    {
+                            sum+=Math.pow((n%10),2);
+                            n=n/10;
+                    }
+                    
+                    n=sum;
+            }
+            return n==1;
+    }
+}
