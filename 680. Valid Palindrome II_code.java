56b0c2ce7f0a144b080e37397c7a2107e26963cc
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-05-09 23:16:39 +0530

    Create 680. Valid Palindrome II_code.java

---
+++ 680. Valid Palindrome II_code.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        boolean ans=check(s,0,s.length()-1,false);
+            return ans;
+    }
+        
+        
+        public boolean check(String s,int l,int h,boolean remove)
+        {
+                
+                while(l <= h){
+                if(s.charAt(l) == s.charAt(h))
+                {
+                        l++;
+                        h--;
+                }
+                else
+                {
+                        if(remove)
+                        {
+                                return false;
+                        }
+                        return check(s,l+1,h,true) || check(s,l,h-1,true);
+                        
+                }
+                
+        }
+        return true;
+}
+}
