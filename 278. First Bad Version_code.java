155984a236a247a72667969f7dd48f472e6b56d1
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-05-18 23:09:29 +0530

    Create 278. First Bad Version_code.java

---
+++ 278. First Bad Version_code.java
@@ -0,0 +1,22 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+            int low=0,high=n;
+            int result=-1;
+            
+            while(low<=high){
+                    
+                    int mid=low+(high-low)/2;
+                    
+                    if(isBadVersion(mid)){
+                            result=mid;
+                            high=mid-1;
+                    }
+                    else
+                            low=mid+1;
+                    
+                   // return result;
+            }
+            return result;
+        
+    }
+}
