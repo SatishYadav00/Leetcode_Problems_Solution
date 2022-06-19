ae263134e0767980e1e758b9c6fe956db5827336
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-06-19 23:02:18 +0530

    Create 1268. Search Suggestions System_code.java

---
+++ 1268. Search Suggestions System_code.java
@@ -0,0 +1,39 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+       List<List<String>> ls = new ArrayList<>();
+		 Arrays.sort(products);
+		
+		//<Product Value, Id in sorted array>
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        List<String> productList = Arrays.asList(products);
+        
+        for(int i = 0; i<products.length; i++){
+            tm.put(products[i], i);
+        }
+        
+        String key = "";
+        for(char ch : searchWord.toCharArray()){
+            key += ch;
+			
+		/*here ceiling value is the least key which is greater than or equal to the given key value If such key is absent, null is returned*/
+           String ceiling = tm.ceilingKey(key);   
+		   
+      /*here floor value is returns the greatest key less than or equal to key, or null if there is no such key.*/
+          String floor = tm.floorKey(key + "{");     //using opening bracket here to cover the complete range of lowercase characters as we have ASCII value of z is 122 & ASCII value of '{' is 123, In order to cover the range starting from 'm' (ATQ) up till the last char of z, basically it will check in this range m<=words<=m{  (here m is first char of our searchword)
+            
+            if(ceiling == null || floor == null)    //this condition will avoid null pointer exception 
+            {
+                break;
+            }           
+            ls.add(productList.subList(tm.get(ceiling), Math.min(tm.get(ceiling)+3,tm.get(floor)+1))); 
+        }
+        
+        while(ls.size()<searchWord.length()){     //if list size is less than the length of our searchword then we return empty list here
+            ls.add(new ArrayList<>());
+        }
+        
+        return ls;
+        
+    
+    }
+}
