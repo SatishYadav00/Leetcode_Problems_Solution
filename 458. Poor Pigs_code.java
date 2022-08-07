6496068fffdb3ff87c99a9632564979194ffaff0
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-08-07 22:40:53 +0530

    Update 458. Poor Pigs_code.java

--- 458. Poor Pigs_code.java
+++ 458. Poor Pigs_code.java
@@ -1,6 +1,7 @@
 class Solution {
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         int x=(minutesToTest)/(minutesToDie)+1;
-        return   (int)Math.ceil(Math.log(buckets)/Math.log(x));
+        int n= (int)Math.ceil(Math.log(buckets)/Math.log(x));
+        return n;
     }
 }
