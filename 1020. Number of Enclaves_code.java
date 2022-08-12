67dec4b94855ce15b5a0ef74b74f5ac9c60f35f7
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-08-12 22:44:24 +0530

    Update 1020. Number of Enclaves_code.java

--- 1020. Number of Enclaves_code.java
+++ 1020. Number of Enclaves_code.java
@@ -21,7 +21,7 @@ class Solution {
         
     }
     
-      public void dfs(int row, int col, int[][] grid) {
+       void dfs(int row, int col, int[][] grid) {
         if(row >= 0 && col >= 0 && row < grid.length && col < grid[0].length && grid[row][col] == 1) {
             if(row == 0 || col == 0 || row == grid.length-1 || col == grid[0].length-1){
                 corr = true;
