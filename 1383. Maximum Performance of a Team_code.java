37544fea37aa7047e4e44ff39fd549f74f42304b
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-09-11 22:55:28 +0530

    Create 1383. Maximum Performance of a Team_code.java

---
+++ 1383. Maximum Performance of a Team_code.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maxPerformance(int n, int[] speed, int[] efficiency, int k) {
+        
+     int MOD = 1000000007;
+	int[][] engineers = new int[n][2];
+	
+	for (int i = 0; i < n; i++)
+		engineers[i] = new int[] {efficiency[i], speed[i]};
+	
+	Arrays.sort(engineers, (a, b) -> b[0] - a[0]); // Descending order of efficiency
+	
+	PriorityQueue<Integer> pq = new PriorityQueue<>(k, (a, b) -> a - b);       
+	long result = Integer.MIN_VALUE, speedSum = 0;
+	
+	for (int[] engineer : engineers) {
+		if (pq.size() == k) 
+			speedSum -= pq.poll(); // Remove the one with min speed
+		
+		pq.add(engineer[1]);
+		speedSum = speedSum + engineer[1];
+		
+		result = Math.max(result, speedSum * engineer[0]);
+	}
+	
+	return (int) (result % MOD);
+    }
+}
+  
