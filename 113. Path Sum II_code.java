66c4ed12c96dfdff9cc6b384cfe7c940c58d0ad7
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-09-24 22:44:50 +0530

    Update 113. Path Sum II_code.java

--- 113. Path Sum II_code.java
+++ 113. Path Sum II_code.java
@@ -1,26 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-   
-         List<List<Integer>> res = new LinkedList<>();
+    List<List<Integer>> res=new LinkedList<>();
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        if(root == null) return res;
-        dfs(root, targetSum, new LinkedList<>());
+        
+        if(root==null)
+            return res;
+        
+        dfs(root,targetSum,new LinkedList<>());
+        
         return res;
     }
-    private void dfs(TreeNode root, int targetSum, List<Integer> path){
-        targetSum -= root.val;
+    
+    private void dfs(TreeNode root,int target,List<Integer> path)
+    {
+        target-=root.val;
         path.add(root.val);
-        //Base case: leaf node
-        if(root.left == null && root.right == null && targetSum == 0){
-            res.add(new LinkedList<>(path));
-        }else{
-            if(root.left != null){
-                dfs(root.left, targetSum, path);
-            }
-            if(root.right != null){
-                dfs(root.right, targetSum, path);
-            }
-        }
         
-        path.remove(path.size() - 1);
+        if(root.right==null && root.left==null && target==0)
+        {
+            res.add(new LinkedList<>(path));
+        }
+        else
+        {
+            if(root.left != null)
+            {
+                dfs(root.left,target,path);
+            }
+            if(root.right != null)
+            {
+        dfs(root.right,target,path);
+}
+        }
+        path.remove(path.size()-1);
+        
     }
 }
