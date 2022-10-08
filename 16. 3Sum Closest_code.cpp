2eff1ef199c8bfcd541df844a85f4e7398f5d3cf
Satish Yadav <70273513+SatishYadav00@users.noreply.github.com>
2022-10-08 22:58:26 +0530

    Update 16. 3Sum Closest_code.cpp

--- 16. 3Sum Closest_code.cpp
+++ 16. 3Sum Closest_code.cpp
@@ -34,3 +34,41 @@ public:
     return out;
     }
 };
+
+// JAVA code
+
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+          int sum,n=nums.length;
+            int out=0;
+            int min=Integer.MAX_VALUE;
+          Arrays.sort(nums);
+            for(int i=0;i<n-2;i++)
+            {
+                    if(i==0 || nums[i] !=nums[i-1])
+                    {
+                            int L=i+1;
+                            int R=n-1;
+                    
+                    while(L<R)
+                    {
+                            sum=nums[i]+nums[L]+nums[R];
+                            if(sum==target)
+                                    return sum;
+                            else if(sum<target)
+                                    L++;
+                            else
+                                    R--;
+                            if(Math.abs(sum-target)<min)
+                            {
+                                    out=sum;
+                                    min=Math.abs(sum-target);
+                            }
+                    }
+            }
+            
+                    
+    }
+    return out;
+    }
+}
